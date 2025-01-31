10df31e818b25ba118cc01021d310432437e89cc
GinaRodriguez <gina.rodriguez@salesforce.com>
2025-01-30 19:08:17 -0800

    adding components

--- src/components/IceCream.jsx
+++ src/components/IceCream.jsx
@@ -1,16 +1,13 @@
 const IceCream = ({ icecreamitem }) => {
   return (
     <div className="ice cream">
-      {/* <img src={props.pictures} /> */}
       <h2>{icecreamitem.name}</h2>
-      <h2> {icecreamitem.colors}</h2>
-      <h2> {icecreamitem.year}</h2>
-      <h2> {icecreamitem.origin}</h2>
+      <h2> Color: {icecreamitem.colors}</h2>
+      <h4> Year: {icecreamitem.year}</h4>
+      <h4> Origin: {icecreamitem.origin}</h4>
       {icecreamitem.pictures.map((image) => (
-        <img src={image.url} />
+        <img src={image.url} class="center" />
       ))}
-      {/* <h2> Name: {props.name}</h2>
-      <h5> Colors: {props.colors}</h5> */}
     </div>
   )
 }
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+import IceCream from './IceCream'
+
+const Navigation = (icecreamitem) => {
+  console.log(icecreamitem)
+}
+;<nav>
+  <ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Company</li>
+    <li>Contact Us</li>
+    <li>Blog</li>
+  </ul>
+</nav>
+
+export default IceCream
