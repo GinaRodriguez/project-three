dfab49f14d5ab2c6a9b9fe3ca1db4049473aadf0
GinaRodriguez <gina.rodriguez@salesforce.com>
2025-02-03 17:40:50 -0800

    navigation styling

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -2,8 +2,6 @@ const Module = () => {
   return (
     <div className="module">
       <h5>Thank you for visiting</h5>
-      <button>Click me</button>
-      {/* <image src={this.props.url} /> */}
     </div>
   )
 }
